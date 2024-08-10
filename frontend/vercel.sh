c0df8e6b911986d9d7a1fdd4120d10ff59daad5c
Suphal Bochkar <suphalbochkar@gmail.com>
2024-08-10 21:57:19 +0530

    Deployment Fix

--- frontend/vercel.sh
+++ frontend/vercel.sh
@@ -2,9 +2,12 @@
 
 echo "VERCEL_ENV: $VERCEL_ENV"
 
-if [[ $VERCEL_ENV == "production" ]] ; then
+if [[ "$VERCEL_ENV" == "production" ]] ; then
   echo "Checking for 'build' in the latest commit..."
   git log -1 --pretty=oneline --abbrev-commit | grep -w "build" && exit 1 || exit 0
+  echo "✅ - Build can proceed"
+  exit 1;
 else
-  exit 1
+  echo "🛑 - Build cancelled"
+  exit 0;
 fi
