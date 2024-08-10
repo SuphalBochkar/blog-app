06d8ed9340c5cc21b72918744c58b108dce8796b
Suphal Bochkar <suphalbochkar@gmail.com>
2024-08-10 21:40:49 +0530

    Deployment Fix

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if [[ $VERCEL_ENV == "production"  ]] ; then
+echo "VERCEL_ENV: $VERCEL_ENV"
+
+if [[ $VERCEL_ENV == "production" ]] ; then
+  echo "Checking for 'build' in the latest commit..."
   git log -1 --pretty=oneline --abbrev-commit | grep -w "build" && exit 1 || exit 0
 else
   exit 1
